9cd4536ec1cd306c672a3eb7d8960cf96451c878
TiKevin83 <travismcgeehan@gmail.com>
2018-08-12 21:43:31 -0400

    Picture tag support for a responsive image demo

--- tikevin83.js
+++ tikevin83.js
@@ -9,14 +9,16 @@
 })();
 
 //All JS after this is unnecessary if you don't need autoplay
-var slidesShown = 0;
-var slideInputs = document.querySelectorAll(".loSlide input");
-var autoplayInterval = setInterval(function () {
-  slidesShown++;
-  slideInputs[slidesShown % slideInputs.length].checked = true;
-  if (slidesShown >= slideInputs.length * 3) clearInterval(autoplayInterval);
-}, 8000);
+(function autoPlay() {
+  var slidesShown = 0;
+  var slideInputs = document.querySelectorAll(".loSlide input");
+  var autoplayInterval = setInterval(function () {
+    slidesShown++;
+    slideInputs[slidesShown % slideInputs.length].checked = true;
+    if (slidesShown >= slideInputs.length * 3) clearInterval(autoplayInterval);
+  }, 8000);
 
-[].forEach.call(slideInputs,function(e){e.addEventListener('click',function () {
-  clearInterval(autoplayInterval);
-},false)})
+  [].forEach.call(slideInputs,function(e){e.addEventListener('click',function () {
+    clearInterval(autoplayInterval);
+  },false)})
+})();
